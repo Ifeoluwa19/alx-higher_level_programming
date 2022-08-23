32525809770e79eed9f7986c42c1b7b9050f0c79
Ifeoluwa19 <ifeoluwabuiz@gmail.com>
2022-08-23 16:59:41 -0700

    advance task

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    if c > b:
-        return a + b
-    return (a * b) - c
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print(i, end=" ")
