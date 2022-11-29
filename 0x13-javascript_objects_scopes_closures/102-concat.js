76014b2e3e91c804c37c0f6db87ef28227e91c98
Ifeoluwa19 <ifeoluwabuiz@gmail.com>
2022-11-29 14:39:52 -0800

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const fs = require('fs');
-
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+// JS Script
+let fs = require('fs');
+let text1 = fs.readFileSync(process.argv[2], 'utf-8');
+let text2 = fs.readFileSync(process.argv[3], 'utf-8');
+fs.appendFile(process.argv[4], text1.concat(text2).trim());
+fs.appendFile(process.argv[4], '\n');
